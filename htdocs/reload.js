80e020634b7956e7979774e522406258e8a02eb9
Aveline Swan <i@xswan.net>
2015-01-25 04:54:41 +0800

    prevent infinite reloading

--- htdocs/reload.js
+++ htdocs/reload.js
@@ -1,1 +1,9 @@
-window.location.reload();
+var count = sessionStorage.getItem(window.location) || 0;
+if(count < 5) {
+  count++;
+  sessionStorage.setItem(window.location, count);
+  
+  window.location.reload();
+} else {
+  sessionStorage.removeItem(window.location);
+}
